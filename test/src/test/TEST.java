c1cbd6047a037315cff2dba66248f53ff6688605
alok2599 <alok2599@gmail.com>
2016-10-08 09:57:27 +0530

    first change.

---
+++ test/src/test/TEST.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+public class TEST {
+//changing for the first time.
+	public static void main(String args[]) throws Exception{
+		//sup s=new sup();
+		Integer n=new Integer(3);
+		new Thread(new MyThread(n)).start();
+		new Thread(new MyThread(n)).start(); 
+		System.out.println(n);
+	}
+}
+ 
+class MyThread implements Runnable{
+
+	Integer i=null;
+	MyThread(Integer ii){
+		i=ii;
+	}
+	@SuppressWarnings("unused")
+	@Override
+	public void run() {
+	i++;
+    System.out.println(i);
+    
+		
+	}
+	
+	
+}
